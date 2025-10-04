7087f335cae0bf057af5acc7d2023cf8e974ff48
Nguyễn Tiến Huy <101089119+huyyyyyyy2007@users.noreply.github.com>
2025-10-04 16:36:47 +0700

    fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -2,18 +2,28 @@
 #include <cstdlib>
 using namespace std;
 int main(){
-       for (int a = 0; a < 100; a++){
-       if ( a % 3==0){
-        cout <<"Fizz";
-       }
-      else if ( a% 5==0){
-        cout << "Buzz";
-      }
-      else {
-        cout << a;
-      }
-       }
+     vector<string> number;
+     int n;
+     cout << "Nhập n: ";
+     cin >> n;
+      for(int i = 1; i < n; i ++){
+        if (i % 3==0 && i % 5 ==0){
+          number.push_back("FizzBuzz");
+        }
+        else if(i % 3==0){
+          number.push_back("Fizz");
+        }
+        else if (i % 5 ==0){
+          number.push_back("Buzz");
+        }
+        else{
+          number.push_back(to_string(i)); // i đang là int nhưng để đưa vào vector thì phải đổi qua string.
+        }
         
+      }
+      for(string element : number){
+        cout << element << endl;
+      }
         
  return 0;
 }
